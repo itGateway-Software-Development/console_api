dd70ba216811d54e388f0e18c324eb3ca31f5d6e
itGateway-Software-Development <developer@itgateway.org>
2024-12-26 13:58:59 +0630

    increate set time limit

--- app/Http/Controllers/Api/DeployController.php
+++ app/Http/Controllers/Api/DeployController.php
@@ -13,6 +13,7 @@ class DeployController extends Controller
     public function deploy($os) {
         ini_set('max_execution_time', 0); // No time limit
         ini_set('memory_limit', '-1');   // Unlimited memory
+        set_time_limit(600);
 
         // logger($os);
         // $scriptPath = $os == 'Ubuntu' ? '/home/ken/Documents/scripts/linux.sh' : '/home/ken/Documents/scripts/window.sh';
